410812a698a98ac8d6b184a063da66169dc7bdec
ChoiMari <chl98639863@gmail.com>
2025-10-10 02:39:04 +0900

    chore : 필터 - 응답인코딩 추가함

--- MovieForYou/src/main/java/kr/or/hamaeyu/filter/EncodingFilter.java
+++ MovieForYou/src/main/java/kr/or/hamaeyu/filter/EncodingFilter.java
@@ -9,10 +9,14 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpFilter;
 import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 모든 요청을 대상으로 문자 인코딩(UTF-8) 필터
+ */
 @Slf4j
 public class EncodingFilter extends HttpFilter {
 	private static final long serialVersionUID = 1L;
-	private String encoding;
+	private String encoding;  // 인코딩 타입 (web.xml <param-value>에서 가져옴)
 	
 	//WAS가 Filter 객체를 생성한 후, 필터의 초기화 작업을 하기 위해서 호출 하는 메서드
 		@Override
@@ -20,6 +24,7 @@ public class EncodingFilter extends HttpFilter {
 			//web.xml에서 <filter>의 <param-name> 값을 아규먼트로 전달하면,
 			//<filter>의 <param-value> 값을 리턴해줌.
 			encoding = filterConfig.getInitParameter("encoding"); //web.xml에 있는 <param-name>encoding</param-name> 이름과 같은걸로 아규먼트로 줘야함
+			//<param-value>UTF-8</param-value>해놓음
 			log.debug("init:encoding={}",encoding);
 		}
 		
@@ -27,10 +32,18 @@ public class EncodingFilter extends HttpFilter {
 		@Override
 		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 				throws IOException, ServletException {
+			
 			//요청(request) 객체의 문자열 인코딩 타입을(UTF-8)로 설정:
 			request.setCharacterEncoding(encoding);
 			
-			//다음 필터 체인을 진행(->서블릿메서드(doGet이나 doPost) 호출)
+			//응답 인코딩 설정
+			response.setCharacterEncoding(encoding);
+			
+			//content-Type은 각 서블릿에서 설정
+			
+			 log.debug("doFilter: request/response encoding set to {}", encoding);
+			
+			//다음 필터 체인을 진행하거나 서블릿 메서드 호출
 			chain.doFilter(request, response);
 		}
 }
