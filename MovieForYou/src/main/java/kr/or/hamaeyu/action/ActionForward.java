070a1fc841613d3dc70a69c010b13afa2adad1fd
ChoiMari <chl98639863@gmail.com>
2025-10-12 06:39:01 +0900

    refactor : 롬복 애너테이션 추가함

--- MovieForYou/src/main/java/kr/or/hamaeyu/action/ActionForward.java
+++ MovieForYou/src/main/java/kr/or/hamaeyu/action/ActionForward.java
@@ -1,9 +1,12 @@
 package kr.or.hamaeyu.action;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @Builder
 public class ActionForward {
 	private boolean isRedirect = false; // 뷰의 전환 여부를 결정
 	private String path = null;//이동 경로의 주소 관리(forward시킬 경로)
