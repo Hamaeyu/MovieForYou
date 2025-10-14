9678e8eb9f34862ae991d4754dfa7a94b74a1694
ChoiMari <chl98639863@gmail.com>
2025-10-15 04:46:38 +0900

    feat : 에디터 이미지 업로드 비동기 처리에 사용할 요청/응답 DTO정의

---
+++ MovieForYou/src/main/java/kr/or/hamaeyu/dto/ImageUploadRequest.java
@@ -0,0 +1,21 @@
+package kr.or.hamaeyu.dto;
+
+import java.time.LocalDateTime;
+
+import jakarta.servlet.http.Part;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * 에디터로 이미지 업로드 요청 처리 dto(비동기 요청) 
+ */
+@Getter @NoArgsConstructor @AllArgsConstructor @Builder 
+@EqualsAndHashCode @ToString
+public class ImageUploadRequest {
+	private Part upload; //서블릿에서는 파일을 Part로 받는다고 함
+	private String tempUuid; //글 단위로 프론트에서 생성해서 요청에 넣어주는 UUID
+}
