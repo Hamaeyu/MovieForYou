60e50ad61869defa8944d91a3e0440d326045171
ChoiMari <chl98639863@gmail.com>
2025-10-16 02:29:15 +0900

    docs : 주석 수정

--- MovieForYou/src/main/java/kr/or/hamaeyu/exception/AuthenticationException.java
+++ MovieForYou/src/main/java/kr/or/hamaeyu/exception/AuthenticationException.java
@@ -1,7 +1,7 @@
 package kr.or.hamaeyu.exception;
 /**
  * 커스텀 예외 클래스 
- * 서비스에서 로그인 실패 시 컨트롤러로 던지는데 사용함
+ * 인증 관련 예외로 사용
  */
 public class AuthenticationException extends RuntimeException {
 	public AuthenticationException(String message) {
