4db41445bb086a871a9c16fbb5590c0795a19c05
ChoiMari <chl98639863@gmail.com>
2025-10-16 15:24:26 +0900

    feat : 상태코드 추가함

--- MovieForYou/src/main/java/kr/or/hamaeyu/dto/FreeImageDeleteResponse.java
+++ MovieForYou/src/main/java/kr/or/hamaeyu/dto/FreeImageDeleteResponse.java
@@ -14,4 +14,5 @@ import lombok.ToString;
 public class FreeImageDeleteResponse {
 	private boolean success;// true: 삭제 성공, false: 실패
 	private String message;// 응답으로 보낼 메세지
+	private int status;   // HTTP 상태 코드 (예: 200, 400, 404, 500)
 }
