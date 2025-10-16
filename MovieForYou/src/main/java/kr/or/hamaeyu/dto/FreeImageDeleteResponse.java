abd42f3b5b5ee9ce6d8b900d204449958acc0825
ChoiMari <chl98639863@gmail.com>
2025-10-16 13:43:53 +0900

    feat : 업로드 삭제 요청/응답 DTO

---
+++ MovieForYou/src/main/java/kr/or/hamaeyu/dto/FreeImageDeleteResponse.java
@@ -0,0 +1,17 @@
+package kr.or.hamaeyu.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter @NoArgsConstructor @AllArgsConstructor @Builder 
+@EqualsAndHashCode @ToString
+public class FreeImageDeleteResponse {
+	private boolean success;// true: 삭제 성공, false: 실패
+	private String message;// 응답으로 보낼 메세지
+}
